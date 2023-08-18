a155da4a874cb9479f150e0cff11bdbea5293e6e
tatiananeto <alielneto@gmail.com>
2023-08-18 10:07:14 +0100

    csharp

---
+++ aulas/aula05/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    class Aula05 { 
+         static void Main()
+          {
+            //int res = (10 + 5) *2;
+            //bool res = 10 != 5;
+            int num = 10;
+            num++;//num += 1;//num = num + 1;
+            num += 2;//num=num+2; posso usar para soma, multiplicao, subtracao e divisao
+
+            bool res = (5 > 3) & (9>4);
+            Console.WriteLine(res);
+          }
+    }
+}
---
+++ aulas/aula06/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*int n1, n2, n3;
+
+        n1 = 10; n2 = 20; n3 = 30;
+
+        Console.Write("\nn1=\t{0},\nn2=\t{1}, \nn3=\t{2}", n1, n2, n3); // modelo de string aonde eu nformo a posicao de cada numero entrechavetas, os indices sempre começam de 0
+        // \n qurebra linha, \t cria tabulação*/
+
+        double valorCompra = 5.50;
+        double valorVenda;
+        double lucro=0.1;
+        string produto = "pastel";
+
+        valorVenda = valorCompra + (valorCompra * lucro);
+
+        Console.WriteLine("Produto.......:{0,15}", produto);
+        Console.WriteLine("Val.compra....:{0,15:c}", valorCompra);
+        Console.WriteLine("Lucro.........:{0,15:p}", lucro);
+        Console.WriteLine("Val.venda.....:{0,15:c}", valorVenda);
+    }
+
+}
