4172b918fb82bb02562d7e6a0f9dae1d95f7f771
tatiananeto <alielneto@gmail.com>
2023-09-12 22:26:11 +0100

    tesr

---
+++ aulas/ex01/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int day;
+        Console.Write("Digite o dia: ");
+        day = Convert.ToInt32(Console.ReadLine());
+        switch (day)
+        {
+            case 1:
+                Console.WriteLine("Monday");
+                break;
+            case 2:
+                Console.WriteLine("Tuesday");
+                break;
+            case 3:
+                Console.WriteLine("Wednesday");
+                break;
+            case 4:
+                Console.WriteLine("Thursday");
+                break;
+            case 5:
+                Console.WriteLine("Friday");
+                break;
+            case 6:
+                Console.WriteLine("Saturday");
+                break;
+            case 7:
+                Console.WriteLine("Sunday");
+                break;
+        }
+        var valor = int.Parse("123");
+        var valor2 = bool.Parse("true");
+        var valor3 = DateTime.Parse("01/01/2022");
+
+        Console.WriteLine(valor);
+        Console.WriteLine(valor2);
+        Console.WriteLine(valor3);
+    }
+}
